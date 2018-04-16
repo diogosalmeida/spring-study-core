4cfc0c85176c563e10c40f881692c5437c98ba59
Diogo Almeida <diogomean@gmail.com>
2018-04-15 22:47:50 -0300

    Config context xml

---
+++ src/main/java/com/study/spring/Application.java
@@ -0,0 +1,16 @@
+package com.study.spring;
+
+import com.study.spring.service.CustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args){
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
+
+        System.out.println(service.findAll().get(0).getFirstname());
+    }
+}
--- src/main/java/com/study/spring/service/CustomerServiceImpl.java
+++ src/main/java/com/study/spring/service/CustomerServiceImpl.java
@@ -2,14 +2,21 @@ package com.study.spring.service;
 
 import com.study.spring.model.Customer;
 import com.study.spring.repository.CustomerRepository;
-import com.study.spring.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
 
-    public List<Customer> findAll(){
-       return customerRepository.findAll();
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
     }
+
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
 }
